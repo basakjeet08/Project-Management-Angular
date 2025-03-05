4fb7d4d203b2a9e40b1c98f00d5752fd6ac2f3df
Anirban Basak <jeetbasak2002@gmail.com>
2025-03-05 16:49:41 +0530

    feat : Implemented the UI for the login screen.

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,8 +1,31 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {}
+export class LoginComponent {
+  // This variable is the data input from the user
+  userInput = {
+    username: '',
+    password: '',
+  };
+
+  // This error message is used to show errors coming from the Api Calls
+  errorMessage: string | null = null;
+
+  // Injecting the needed dependecies in the constructor
+  constructor(private router: Router, private route: ActivatedRoute) {}
+
+  // This function is invoked when the user clicks on the submit Buttonm
+  onSubmitClick() {
+    console.log(this.userInput);
+  }
+
+  // This function is invoked when the user clicks on the go to register Button
+  goToRegisterPageClick() {
+    this.router.navigate(['../', 'register'], { relativeTo: this.route });
+  }
+}
