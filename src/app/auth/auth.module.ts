e938f08048cb2fc45a2aebd0ed6762454972d8d3
Anirban Basak <jeetbasak2002@gmail.com>
2025-03-05 16:42:28 +0530

    feat : Implemented the register user page UI.

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,6 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './auth.component';
 import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
+import { FormsModule } from '@angular/forms';
 
 // These are the routes for the auth module
 const authRoutes: Routes = [
@@ -20,6 +21,6 @@ const authRoutes: Routes = [
 
 @NgModule({
   declarations: [AuthComponent, RegisterComponent, LoginComponent],
-  imports: [CommonModule, RouterModule.forChild(authRoutes)],
+  imports: [CommonModule, RouterModule.forChild(authRoutes), FormsModule],
 })
 export class AuthModule {}
--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,8 +1,33 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css'],
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+  // This is the variable which stores the user input data
+  userInput = {
+    firstname: '',
+    lastname: '',
+    username: '',
+    password: '',
+  };
+
+  // This variable contains the error message for the api calls and the form input
+  errorMessage: string | null = null;
+
+  // Injecting the necessary things through the constructor
+  constructor(private router: Router, private route: ActivatedRoute) {}
+
+  // This function is invoked when the user clicks on the submit Button
+  onSubmitClick() {
+    console.log(this.userInput);
+  }
+
+  // This function takes you to the login page
+  onGoToLoginPageClick() {
+    this.router.navigate(['../', 'login'], { relativeTo: this.route });
+  }
+}
